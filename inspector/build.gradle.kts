8669887380b633459f04dea5b82bdbd499040fe2
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-05-20 16:10:28 -0400

    Make a subbuild/project

---
+++ inspector/build.gradle.kts
@@ -0,0 +1,26 @@
+
+
+var inspector = sourceSets.create("inspector") {
+    java.srcDir("src/inspector/java")
+}
+
+tasks {
+    val inspectorJar = register<Jar>("buildInspector") {
+        group = "build"
+        description = "Build JAR for Inspector"
+        archiveBaseName = "inspector"
+
+        manifest {
+            attributes("Main-Class" to "klaxon.klaxon.inspector.Inspector")
+        }
+
+        from(inspector.output)
+    }
+
+    register<JavaExec>("runInspector") {
+        group = "build"
+        description = "Run Inspector"
+
+        classpath = files(inspectorJar)
+    }
+}
