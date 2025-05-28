4703bc9e3144adf25fe1dcbafec952ca87dbee77
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-05-28 11:51:27 -0400

    Update Java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+minecraft {
+    javaCompatibilityVersion = 24
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
+}
