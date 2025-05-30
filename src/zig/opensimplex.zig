5a6b8f8f9f5ac99cc8570d8b24a732261edc6c7d
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-05-30 16:48:26 -0400

    licence headers

--- src/zig/opensimplex.zig
+++ src/zig/opensimplex.zig
@@ -1,5 +1,19 @@
-// Translated to Zig, from the Rust version of OpenSimplex2F here https://github.com/KdotJPG/OpenSimplex2/
-// Licensed under the LGPLv3, see main license file
+// This file is part of Cursor - a mod that _runs_.
+// Copyright (C) 2025 ah-OOG-ah
+//
+// Cursor is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Cursor is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 const std = @import("std");
 
 const PRIME_X: i64 = 0x5205402B9270C86F;
